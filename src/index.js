5bc4749e2a653c94bc9dc3712909c0b718053496
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-03-18 00:31:31 +0300

    created background and ground

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // Phaser
 import Phaser from 'phaser';
 
+// Assets
+import bg from './assets/bg.png';
+import ball from './assets/blue_ball.png';
+import ground from './assets/platform.png';
+import tomato from './assets/tomato.png';
+
 // Phaser config
 const config = {
   type: Phaser.AUTO,
@@ -19,8 +25,22 @@ const config = {
 // eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image('bg', bg);
+  this.load.image('ground', ground);
+}
 
-function create() {}
+let platforms;
+function create() {
+  this.add.image(400, 300, 'bg');
+  platforms = this.physics.add.staticGroup();
+  platforms
+    .create(400, 568, 'ground')
+    .setScale(2)
+    .refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+}
 
 function update() {}
