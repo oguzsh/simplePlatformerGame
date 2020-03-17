a16a694928bf1336f357cbabc11e8490d59e4b73
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-03-18 00:44:06 +0300

    created player

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import Phaser from 'phaser';
 
 // Assets
 import bg from './assets/bg.png';
-import ball from './assets/blue_ball.png';
+import playerSprite from './assets/player.png';
 import ground from './assets/platform.png';
-import tomato from './assets/tomato.png';
 
 // Phaser config
 const config = {
@@ -28,9 +27,15 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image('bg', bg);
   this.load.image('ground', ground);
+  this.load.spritesheet('player', playerSprite, {
+    frameWidth: 32,
+    frameHeight: 48
+  });
 }
 
 let platforms;
+let player;
+
 function create() {
   this.add.image(400, 300, 'bg');
   platforms = this.physics.add.staticGroup();
@@ -41,6 +46,36 @@ function create() {
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  // Player
+  player = this.physics.add.sprite(100, 450, 'player');
+
+  // Add Collider
+  this.physics.add.collider(player, platforms);
+
+  player.setBounce(0.2);
+  player.body.setGravityY(300);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [{ key: 'player', frame: 4 }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 }
 
 function update() {}
